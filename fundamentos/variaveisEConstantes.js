d353fcec6d1895186917f8e339e81e0753cad18c
Yitshhaq Fukushima <ymotse@gmail.com>
2020-05-23 18:15:21 -0300

    15. O basico de Var, Let, e Const

---
+++ fundamentos/variaveisEConstantes.js
@@ -0,0 +1,19 @@
+/**
+ * 15. O basico de Var, Let, e Const
+ */
+
+var a = 3
+let b = 4
+
+var a = 30
+b = 40
+
+console.log(a, b)
+
+a = 300
+b = 400
+
+const c = 5
+
+console.log(a, b)
+console.log(c)
