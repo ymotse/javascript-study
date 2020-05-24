fd4c182a76b912be81aebadd0d15ce81ecdc3cc4
Yitshhaq Fukushima <ymotse@gmail.com>
2020-05-24 13:52:40 -0300

    20. Tipos em Javascript: String

---
+++ fundamentos/strings.js
@@ -0,0 +1,14 @@
+const escola = "Cod3r"
+
+console.log(escola.charAt(4))
+console.log(escola.charAt(50))
+console.log(escola.charCodeAt(3))
+console.log(escola.indexOf('d'))
+
+console.log(escola.substring(1))
+console.log(escola.substring(0, 3))
+
+console.log('Escola '.concat(escola).concat("!"))
+console.log(escola.replace(3,'e'))
+
+console.log('Ana, Bia, Lia, Gui'.split(','))
