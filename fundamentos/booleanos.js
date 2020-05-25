9faeb54413f562b75d0638af724cba11809e5976
Yitshhaq Fukushima <ymotse@gmail.com>
2020-05-25 12:05:48 -0300

    22. Tipos em Javascript: Boolean

---
+++ fundamentos/booleanos.js
@@ -0,0 +1,37 @@
+let isAtivo = false
+console.log(isAtivo)
+
+
+isAtivo = true
+console.log(isAtivo)
+
+isAtivo = 1
+console.log(!!isAtivo)
+
+console.log('---------------------')
+console.log('os tipos verdadeiros:')
+console.log(!!3)
+console.log(!!-1)
+
+console.log(!!' ')
+console.log(!![])
+console.log(!!{})
+console.log(!!Infinity)
+console.log(!!(isAtivo = true))
+console.log(!!(isAtivo = Infinity))
+
+console.log()
+console.log('os tipos falsos:')
+console.log(!!0)
+console.log(!!'')
+console.log(!!null)
+console.log(!!NaN)
+console.log(!!undefined)
+console.log(!!(isAtivo = false))
+
+let nome1 = ''
+console.log(nome1 || 'Desconhecido1')
+
+let nome2 = 'Lucas'
+console.log(nome2 || 'Desconhecido2')
+
