77f69eb2abe801b7bb9601f81bfd6bb5e6543114
Yitshhaq Fukushima <ymotse@gmail.com>
2020-05-28 22:09:06 -0300

    28. Exemplos básico de funções #02

---
+++ fundamentos/funcao2.js
@@ -0,0 +1,20 @@
+// 28. Exemplos básico de funções #02
+
+const imprimirSoma = function(a, b) {
+    console.log(a + b)
+}
+
+imprimirSoma(2, 3)
+
+
+const soma = (a, b) => {
+    return a + b
+}
+
+console.log(soma(3, 3))
+
+
+const subtracao = (a, b) => a - b
+
+console.log(subtracao(3, 4))
+
