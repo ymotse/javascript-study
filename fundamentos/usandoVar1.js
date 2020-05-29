e2c59f081238ed8396a14f8483d1a17f8cfd0506
Yitshhaq Fukushima <ymotse@gmail.com>
2020-05-28 22:13:33 -0300

    29. Declaração de variáveis com Var #01

---
+++ fundamentos/usandoVar1.js
@@ -0,0 +1,24 @@
+// 29. Declaração de variáveis com Var #01
+
+{
+    {
+        {
+            {
+                var sera = 'Será???'
+            }
+        }
+    }
+}
+
+console.log(sera)
+
+
+function teste() {
+    var local = 123
+    console.log(local)
+}
+
+teste()
+//console.log(local) // Erro! Nao conseguirá acessar variavel
+
+
