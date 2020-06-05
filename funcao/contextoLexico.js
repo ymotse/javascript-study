0df6a7325464d7a057f97704795544cb4e7ee3d3
Yitshhaq Fukushima <ymotse@gmail.com>
2020-06-05 18:35:18 -0300

    80. Contexto Léxico.

---
+++ funcao/contextoLexico.js
@@ -0,0 +1,14 @@
+// 80. Contexto Léxico.
+
+const valor = 'Global'
+
+function minhaFuncao() {
+    console.log(valor)
+}
+
+function exec() {
+    const valor = 'Local'
+    minhaFuncao()
+}
+
+exec()
