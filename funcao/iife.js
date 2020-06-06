26fb643b4bf9a754fb1b6b70936cf79aa9e6a1ee
Yitshhaq Fukushima <ymotse@gmail.com>
2020-06-06 10:37:26 -0300

    86. IIFE -> Função auto invocada.

---
+++ funcao/iife.js
@@ -0,0 +1,9 @@
+// 86. IIFE
+
+// IIFE -> Immediately Invoked Function Expression
+// Função auto invocada
+
+(function() {
+    console.log('Será executado na hora!')
+    console.log('Foge do escopo mais abrangente!')
+})()
