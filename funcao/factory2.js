339734f8879c6003407516a62150503562745734
Yitshhaq Fukushima <ymotse@gmail.com>
2020-06-06 10:22:55 -0300

    83. Função Factory #02

---
+++ funcao/factory2.js
@@ -0,0 +1,12 @@
+// 83. Função Factory #02
+
+function criarProduto(nome, preco) {
+    return {
+        nome, 
+        preco,
+        desconto: 0.1
+    }
+}
+
+console.log(criarProduto('Notebook', 2199.49))
+console.log(criarProduto('iPad', 1199.49))
