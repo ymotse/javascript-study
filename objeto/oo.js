e956e8271fab286870bff5042c8012884f36a0dd
Yitshhaq Fukushima <ymotse@gmail.com>
2020-06-07 16:40:24 -0300

    89. Introdução à OO.

---
+++ objeto/oo.js
@@ -0,0 +1,23 @@
+// 89. Introdução à OO.
+// Código não executável
+
+//Procedural
+processamento(valor1, valor2, valor3)
+
+// OO
+objeto = {
+    valor1, 
+    valor2, 
+    valor3,
+    processamento() {
+        // ...
+    }
+}
+
+objeto.processamento() // Foco passou a ser o objeto
+
+// Principios importantes:
+// 1. abstrações
+// 2. encapsulamento
+// 3. herança (prototype)
+// 4. polimorfismo
