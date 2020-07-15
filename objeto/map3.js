7482ae3d3baef671feca87e7e75097f9333f76cc
Yitshhaq Fukushima <ymotse@gmail.com>
2020-07-15 20:23:04 -0300

    113. Map #03

---
+++ objeto/map3.js
@@ -0,0 +1,27 @@
+// 113. Map #03
+
+Array.prototype.map2 = function(callback) {
+    const newArray = []
+    
+    for(let i = 0; i < this.length; i++) {
+        newArray.push(callback(this[i], i, this))
+    }
+    return newArray
+}
+
+const carrinho = [
+    '{ "nome" : "Borracha", "preco" : 3.45 }',
+    '{ "nome" : "Caderno", "preco" : 13.90 }',
+    '{ "nome" : "Kit de Lápis", "preco" : 41.22 }',
+    '{ "nome" : "Caneta", "preco" : 7.50 }'
+]
+
+// Retornar um array apenas com os precos
+
+const precos = []
+
+const paraObjeto = json => JSON.parse(json)
+const apenasPreco = produto => produto.preco
+
+const resultado = carrinho.map2(paraObjeto).map2(apenasPreco)
+console.log(resultado)
