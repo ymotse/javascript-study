ed2147b6aeae7abd5cb6367a42a83ab95411cf0f
Yitshhaq Fukushima <ymotse@gmail.com>
2020-07-26 12:09:15 -0300

    128. Desafio usando axios

--- node/funcionarios/funcionarios.js
+++ node/funcionarios/funcionarios.js
@@ -1,7 +1,21 @@
 const url = 'http://files.cod3r.com.br/curso-js/funcionarios.json'
 const axios = require('axios')
 
+const chineses = funcionario => funcionario.pais === 'China'
+const mulheres = funcionario => funcionario.genero === 'F'
+const menorSalario = (funcionario, funcionarioAtual) => {
+    return funcionario.salario < funcionarioAtual.salario ? funcionario : funcionarioAtual
+}
+
 axios.get(url).then(response => {
     const funcionarios = response.data
-    console.log(funcionarios)
+    // console.log(funcionarios)
+    
+    //Mulher chinesa com menor salario:
+    const funcionario = funcionarios
+        .filter(chineses)
+        .filter(mulheres)
+        .reduce(menorSalario)
+        
+    console.log(funcionario)
 })
