c3c3b11bc471afb3a8af007e2e5ad9cc3797fc8e
Yitshhaq Fukushima <ymotse@gmail.com>
2020-07-30 13:20:58 -0300

    134. Lendo Arquivos

---
+++ node/arquivoLeitura.js
@@ -0,0 +1,29 @@
+// 134. Lendo Arquivos
+
+const fs = require('fs')
+
+const caminho = __dirname + '/arquivo.json'
+
+
+// sincrono...
+const conteudo = fs.readFileSync(caminho, 'utf-8')
+console.log(conteudo)
+
+
+// assincrono...
+fs.readFile(caminho, 'utf-8', (err, conteudo) => {
+    const config = JSON.parse(conteudo)
+    console.log(`${config.db.host}:${config.db.port}`)
+})
+
+
+
+const config = require('./arquivo.json')
+console.log(config)
+
+
+
+fs.readdir(__dirname, (err, arquivos) => {
+    console.log('Conteudo da pasta...')
+    console.log(arquivos)
+})
