128404a80db8d550b327ea3cbcac2ad8caed282a
Yitshhaq Fukushima <ymotse@gmail.com>
2020-07-30 13:24:08 -0300

    135. Escrevendo Arquivos

---
+++ node/arquivoEscrita.js
@@ -0,0 +1,13 @@
+// 135. Escrevendo Arquivos
+
+const fs = require('fs')
+
+const produto = {
+    nome: 'Celular',
+    preco: 1249.99,
+    desconto: 0.15
+}
+
+fs.writeFile(__dirname + '/arquivoGerado.json', JSON.stringify(produto), err => {
+    console.log(err || 'Arquivo salvo!')
+})
