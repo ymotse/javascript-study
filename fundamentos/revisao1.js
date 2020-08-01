be83d2ed329d10b17f1545d3efe43187fa256873
Yitshhaq Fukushima <ymotse@gmail.com>
2020-07-31 23:13:03 -0300

    145. Revisão: escopo variavel, Template String, Destructuring.

---
+++ fundamentos/revisao1.js
@@ -0,0 +1,24 @@
+// 145. Revisão 01
+{
+    var a = 2
+    let b = 3
+    console.log(b)
+}
+
+console.log(a)
+
+
+// Template String:
+const produto = 'iPad'
+console.log(`${produto} é caro!`)
+
+
+// Destructuring:
+const [l, e, ...tras] = 'Cod3r'
+console.log(l, e, tras)
+
+const [x, y] = [1,2,3]
+console.log(x, y)
+
+const { idade: i, nome } = { nome: 'Ana', idade: 9 }
+console.log(i, nome)
