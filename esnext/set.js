e5b21e68f50b66cf3dae1fe52e319f17f34b42dc
Yitshhaq Fukushima <ymotse@gmail.com>
2020-08-23 15:14:28 -0300

    152. Set

---
+++ esnext/set.js
@@ -0,0 +1,21 @@
+// 152. Set
+
+// Não aceita repetição/não indexada
+
+const times = new Set()
+times.add('Vasco')
+times.add('São Paulo').add('Palmeiras').add('Corinthians')
+times.add('Flamengo')
+times.add('Vasco')
+
+console.log(times)
+console.log(times.size)
+console.log(times.has('vasco'))
+console.log(times.has('Vasco'))
+times.delete('Flamengo')
+console.log(times.has('Flamengo'))
+console.log()
+
+const nomes = ['Raquel', 'Lucas', 'Julia', 'Lucas']
+const nomesSet = new Set(nomes)
+console.log(nomesSet)
