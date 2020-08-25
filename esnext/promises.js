dcdf360294c3575cf72a25d33daf8416b6291684
Yitshhaq Fukushima <ymotse@gmail.com>
2020-08-25 11:18:30 -0300

    154. Promises

---
+++ esnext/promises.js
@@ -0,0 +1,16 @@
+// 154. Promises
+
+function falarDepoisDe(segundos, frase) {
+    return new Promise((resolve, reject) => {
+        setTimeout(() => {
+            resolve(frase)
+            // reject(frase)
+        }, segundos * 1000)
+    })
+}
+
+falarDepoisDe(3, 'Que legal!')
+    .then(frase => frase.concat(' ?!?'))
+    .then(outraFrase => console.log(outraFrase))
+    .catch(e => console.log(e))
+    
