a03366ade54845a1aa18043cc93a20b58529b4fd
Yitshhaq Fukushima <ymotse@gmail.com>
2020-08-27 20:48:07 -0300

    166. Async/Await #01

---
+++ esnext/asyncAwair_revisao1.js
@@ -0,0 +1,49 @@
+// 166. Async/Await #01
+
+function esperarPor(tempo = 2000) {
+    return new Promise(function(resolve) {
+        setTimeout(() => {
+            resolve()
+        }, tempo)
+    })
+}
+
+/*
+esperarPor(2000)
+    .then(() => console.log('Executando promise 1...'))
+    .then(esperarPor)
+    .then(() => console.log('Executando promise 2...'))
+    .then(esperarPor)
+    .then(() => console.log('Executando promise 3...'))
+*/
+
+function retornarValor() {
+    return new Promise(resolve => {
+        setTimeout(() => resolve(10), 2000)
+    })
+}
+
+async function executar() {
+    let valor = await retornarValor()
+    
+    await esperarPor(1500)
+    console.log(`Async/Await ${valor}...`)
+    
+    await esperarPor(1500)
+    console.log(`Async/Await ${valor + 1}...`)
+    
+    await esperarPor(1500)
+    console.log(`Async/Await ${valor + 2}...`)
+    
+    return valor + 3
+}
+
+// executar().then(console.log)
+
+
+async function executarDeVerdade() {
+    const valor = await executar()
+    console.log(valor)
+}
+
+executarDeVerdade()
