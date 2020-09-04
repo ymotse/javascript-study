7e6fde5260ee599aefd6193ea9e571812dd2335e
Yitshhaq Fukushima <ymotse@gmail.com>
2020-09-04 12:02:48 -0300

    195. Formulário #03 com backend Express

--- webhtml/exercicios/form/server.js
+++ webhtml/exercicios/form/server.js
@@ -7,7 +7,13 @@ app.use(bodyParser.urlencoded({ extended: true }))
 
 app.post('/usuarios', (req, res) => {
     console.log(req.body)
-    res.send('<h1>Parabéns</h1>')
+    res.send('<h1>Parabéns. Usuário Incluído!</h1>')
+})
+
+app.post('/usuarios/:id', (req, res) => {
+    console.log(req.params.id)
+    console.log(req.body)
+    res.send('<h1>Parabéns. Usuário Alterado!</h1>')
 })
 
 app.listen(3003)
