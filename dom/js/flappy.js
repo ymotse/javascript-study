17fb174cfba3d4016afb676e27ddd205d54cf29f
Yitshhaq Fukushima <ymotse@gmail.com>
2020-09-23 21:04:10 -0300

    256.  Flappy Bird #06

--- dom/js/flappy.js
+++ dom/js/flappy.js
@@ -115,6 +115,33 @@ function Progresso() {
 }
 
 
+function estaoSobrepostos(elementoA, elementoB) {
+    const a = elementoA.getBoundingClientRect()
+    const b = elementoB.getBoundingClientRect()
+    
+    const horizontal = a.left + a.width >= b.left
+        && b.left + b.width >= a.left
+    
+    const vertical = a.top + a.height >= b.top
+        && b.top + b.height >= a.top
+        
+    return horizontal && vertical
+}
+
+function colidiu(passaro, barreiras) {
+    let colidiu = false
+    
+    barreiras.pares.forEach(ParDeBarreiras => {
+        if(!colidiu) {
+            const superior = ParDeBarreiras.superior.elemento
+            const inferior = ParDeBarreiras.inferior.elemento
+            colidiu = estaoSobrepostos(passaro.elemento, superior)
+                || estaoSobrepostos(passaro.elemento, inferior)
+        }
+    })
+    return colidiu
+}
+
 function FlappyBird() {
     let pontos = 0
     
@@ -123,7 +150,7 @@ function FlappyBird() {
     const largura = areaDoJogo.clientWidth
     
     const progresso = new Progresso()
-    const barreiras = new Barreiras(altura, largura, 200, 400, 
+    const barreiras = new Barreiras(altura, largura, 250, 400, 
         () => progresso.atualizarPontos(++pontos))
     const passaro = new Passaro(altura)
     
@@ -136,6 +163,10 @@ function FlappyBird() {
         const temporizador = setInterval(() => {
             barreiras.animar()
             passaro.animar()
+            
+            if(colidiu(passaro, barreiras)) {
+                clearInterval(temporizador)
+            }
         }, 20)
     }
 }
