839a3145489b936fa0ce4893263275e74809e04e
Yitshhaq Fukushima <ymotse@gmail.com>
2020-10-06 13:09:22 -0300

    290 291. Processando SASS com Gulp

---
+++ gulp/css/gulpfile.js
@@ -0,0 +1,21 @@
+const { series, parallel } = require('gulp')
+const gulp = require('gulp')
+const sass = require('gulp-sass')
+const uglifycss = require('gulp-uglifycss')
+const concat = require('gulp-concat')
+
+function TransformacaoCSS() {
+    return gulp.src('src/sass/index.scss')
+    .pipe(sass().on('error', sass.logError))
+    // .pipe(uglifycss({ "uglyComments" : true }))
+    .pipe(concat('estilo.min.css'))
+    .pipe(gulp.dest('build/css'))
+}
+
+function copiarHTML() {
+    return gulp.src('src/index.html')
+        .pipe(gulp.dest('build'))
+}
+
+// exports.default = parallel(TransformacaoCSS, copiarHTML)
+exports.default = series(TransformacaoCSS, copiarHTML)
