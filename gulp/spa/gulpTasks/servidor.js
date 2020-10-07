07644d1e09060a02c2f6ee2abba4676816a0d0dc
Yitshhaq Fukushima <ymotse@gmail.com>
2020-10-07 20:30:11 -0300

    296. Projeto SPA: Tasks Servidor

--- gulp/spa/gulpTasks/servidor.js
+++ gulp/spa/gulpTasks/servidor.js
@@ -1,10 +1,22 @@
 const gulp = require('gulp')
+const webserver = require('gulp-webserver')
+const watch = require('gulp-watch')
 
-function monitorarArquivos(cb) {
-    return cb()
+function monitorarArquivos() {
+    return gulp.src('build')
+        .pipe(webserver({
+            port: 7070,
+            open: true,
+            livereload: true
+        }))
 }
 
 function servidor(cb) {
+    watch('src/**/*.html', () => gulp.series('appHTML')())
+    watch('src/**/*.scss', () => gulp.series('appCSS')())
+    watch('src/**/*.js', () => gulp.series('appJS')())
+    watch('src/assets/imgs/**/*.*', () => gulp.series('appIMG')())
+    
     return cb()
 }
 
