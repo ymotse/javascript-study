310d061e023278c2ec93c78722aa49d797c22012
Yitshhaq Fukushima <ymotse@gmail.com>
2020-10-09 08:46:46 -0300

    302. Configurando a Pasta de Saída

--- webpack/webpack.config.js
+++ webpack/webpack.config.js
@@ -3,5 +3,9 @@ const webpack = require('webpack')
 module.exports = {
     // mode: 'development',
     mode: 'production',
-    entry: './src/principal.js'
+    entry: './src/principal.js',
+    output: {
+        filename: 'principal.js',
+        path: __dirname + '/public'
+    }
 }
