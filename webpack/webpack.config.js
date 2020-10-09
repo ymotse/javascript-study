24f72967bc0bb541b84b4b7e003d2719dd8d21c1
Yitshhaq Fukushima <ymotse@gmail.com>
2020-10-09 09:42:31 -0300

    306. Carregando Arquivo .SCSS

--- webpack/webpack.config.js
+++ webpack/webpack.config.js
@@ -16,7 +16,7 @@ module.exports = {
     ],
     module: {
         rules: [{
-            test: /\.css$/,
+            test: /\.s?[ac]ss$/,
             use: [
                 MiniCssExtractPlugin.loader,
                 // 'style-loader', // Adiciona CSS a DOM injetando a tag <style>
