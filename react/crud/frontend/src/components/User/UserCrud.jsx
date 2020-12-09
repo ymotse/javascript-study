78268bb799a0d0f7d728a517c7747efe9ca1e7a5
Yitshhaq Fukushima <ymotse@gmail.com>
2020-12-09 12:15:01 -0300

    391. Componente Cadastro Usuário #03

--- react/crud/frontend/src/components/User/UserCrud.jsx
+++ react/crud/frontend/src/components/User/UserCrud.jsx
@@ -31,7 +31,6 @@ export default class UserCrud extends Component {
                 const list = this.getUpdatedList(resp.data)
                 this.setState({ user: initialState.user, list })
             })
-        
     }
     
     getUpdatedList(user) {
@@ -40,10 +39,65 @@ export default class UserCrud extends Component {
         return list
     }
     
+    updateField(event) {
+        const user = { ...this.state.user }
+        user[event.target.name] = event.target.value
+        this.setState({ user })
+    }
+    
+    renderForm() {
+        return (
+            <div className="form">
+                <div className="row">
+                    <div className="col-12 col-md-6">
+                        <div className="form-group">
+                            <label>Name</label>
+                            <input type="text" className="form-control" name="name"
+                                value={this.state.user.name} 
+                                onChange={e => this.updateField(e)}
+                                placeholder="Digite o nome..."
+                            />
+                        </div>
+                    </div>
+                    <div className="col-12 col-md-6">
+                        <div className="form-group">
+                            <label>E-mail</label>
+                            <input type="text" className="form-control" name="email"
+                                value={this.state.user.email} 
+                                onChange={e => this.updateField(e)}
+                                placeholder="Digite o e-mail..."
+                            />
+                        </div>
+                    </div>
+                </div>
+                
+                <hr/>
+                
+                <div className="row">
+                    <div className="col-12 d-flex justify-content-end">
+                        <button 
+                            className="btn btn-primary" 
+                            onClick={e => this.save(e)}
+                        >
+                            Salvar
+                        </button>
+                        
+                        <button 
+                            className="btn btn-secondary ml-2" 
+                            onClick={e => this.clear(e)}
+                        >
+                            Cancelar
+                        </button>
+                    </div>
+                </div>
+            </div>
+        )
+    }
+    
     render() {
         return (
             <Main {...headerProps}>
-                Cadastro de Usuário
+                {this.renderForm()}
             </Main>
         )
     }
