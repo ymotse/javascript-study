9980aaa6aed59c4e28f51e3c5b8d4ffb4f59c678
Yitshhaq Fukushima <ymotse@gmail.com>
2020-12-29 20:31:16 -0300

    454. Bindings

--- angular/crud/frontend/src/app/views/product-crud/product-crud.component.ts
+++ angular/crud/frontend/src/app/views/product-crud/product-crud.component.ts
@@ -7,9 +7,15 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ProductCrudComponent implements OnInit {
 
+    propLegal = "queLegalID"
+    
   constructor() { }
 
   ngOnInit(): void {
   }
-
+  
+  fazerAlgo() : void {
+      console.log('Fazer Algo!!')
+  }
+  
 }
