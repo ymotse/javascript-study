f2c0fe18f18c57e9a598818d2cd73de961ac4e0a
Yitshhaq Fukushima <ymotse@gmail.com>
2021-01-03 19:32:06 -0300

    525. Tratando a Primeira Requisição

--- exercicios-express/index.js
+++ exercicios-express/index.js
@@ -1,6 +1,10 @@
 const express = require('express')
 const app = express()
 
+app.use('/opa', (req, res) => {
+    res.send('Estou bem!')
+})
+
 app.listen(3000, () => {
     console.log('Backend executando...')
 })
