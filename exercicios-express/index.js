e050f8841cbb4c6a5e28d6f7e4f261d08a9783ee
Yitshhaq Fukushima <ymotse@gmail.com>
2021-01-03 19:37:16 -0300

    526. Tipos de Resposta

--- exercicios-express/index.js
+++ exercicios-express/index.js
@@ -2,7 +2,27 @@ const express = require('express')
 const app = express()
 
 app.use('/opa', (req, res) => {
-    res.send('Estou bem!')
+    
+    res.json({
+        data: [
+            { id: 1, name: 'Ana' },
+            { id: 2, name: 'Bia' },
+            { id: 3, name: 'Carlos' },
+        ],
+        count: 30,
+        skip: 0,
+        status: 200
+    })
+    
+    
+    // res.json({
+    //     name: 'iPod 32Gb',
+    //     price: 1899.90, 
+    //     discount: 0.12
+    // })
+    
+    
+    // res.send('<h1>Estou bem!</h1>')
 })
 
 app.listen(3000, () => {
