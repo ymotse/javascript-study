679518ef515c31ef776dac032e3142f0c647a288
Yitshhaq Fukushima <ymotse@gmail.com>
2021-01-04 15:58:57 -0300

    528. Middleware #02

--- exercicios-express/index.js
+++ exercicios-express/index.js
@@ -1,6 +1,10 @@
 const express = require('express')
 const app = express()
 
+const saudacao = require('./saudacaoMid')
+
+app.use(saudacao('João'))
+
 app.use('/opa', (req, res, next) => {
     console.log('Antes...')
     next()
