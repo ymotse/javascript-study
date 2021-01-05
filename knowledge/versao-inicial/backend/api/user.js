f92927f6e2858418def1d46eed9121e863dd78f8
Yitshhaq Fukushima <ymotse@gmail.com>
2021-01-05 18:56:21 -0300

    545. Obter Usuário por ID

--- knowledge/versao-inicial/backend/api/user.js
+++ knowledge/versao-inicial/backend/api/user.js
@@ -53,5 +53,22 @@ module.exports = app => {
             .catch(err => res.status(500).send(err))
     }
     
-    return { save, get }
+    const getById = (req, res) => {
+        
+        try {
+            if(!req.params.id) throw null
+            if(isNaN(req.params.id)) throw null
+        } catch (msg) {
+            return res.status(400).send(msg)
+        }
+        
+        app.db('users')
+            .select('id', 'name', 'email', 'admin')
+            .where({ id: req.params.id })
+            .first()
+            .then(user => res.json(user))
+            .catch(err => res.status(500).send(err))
+    }
+    
+    return { save, get, getById }
 }
