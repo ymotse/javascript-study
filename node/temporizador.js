d42b7b308012e40a49c468210cbe607944596949
Yitshhaq Fukushima <ymotse@gmail.com>
2021-01-08 18:20:38 -0300

    146. Tarefas Agendadas com Temporizador

---
+++ node/temporizador.js
@@ -0,0 +1,21 @@
+const { truncate } = require('fs')
+const schedule = require('node-schedule')
+
+const tarefa1 = schedule.scheduleJob('*/5 * * * * *', function() {
+    console.log('Executando Tarefa 1!', new Date().getSeconds())
+})
+
+
+setTimeout(function() {
+    tarefa1.cancel()
+    console.log('Cancelando Tarefa 1!')
+}, 20000)
+
+const regra = new schedule.RecurrenceRule()
+regra.dayOfWeek = [new schedule.Range(0,6)]
+regra.hour = 18
+regra.second = 20
+
+const tarefa2 = schedule.scheduleJob(regra, function() {
+    console.log('Executando Tarefa 2!', new Date().getSeconds())
+})
