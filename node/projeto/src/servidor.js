0766ff0a18b6527276ebd538491840147b7e1824
Yitshhaq Fukushima <ymotse@gmail.com>
2021-01-08 18:03:56 -0300

    145 e 146. Node - Projeto: API com Express #2 e #3

--- node/projeto/src/servidor.js
+++ node/projeto/src/servidor.js
@@ -1,19 +1,38 @@
 const porta = 3003
 const express = require('express')
 const app = express()
+const bancoDeDados = require('./bancoDeDados')
+const bodyParser = require('body-parser')
+
+app.use(bodyParser.urlencoded({ extended: true }))
 
 app.get('/produtos', (req, res, next) => {
-    console.log('Middleware 1...')
-    next()
+    res.send(bancoDeDados.getProdutos())
 })
 
-app.use((req, res, next) => {
-    console.log('Middleware 2...')
-    next()
+app.get('/produtos/:id', (req, res, next) => {
+    res.send(bancoDeDados.getProduto(req.params.id))
 })
 
-app.get('/produtos', (req, res, next) => {
-    res.send({ nome: 'Notebook', preco: 143.45 }) //Converter para JSON
+app.post('/produtos', (req, res) => {
+    const produto = bancoDeDados.salvarProduto({
+        nome: req.body.name,
+        preco: req.body.price
+    })
+    res.send(produto)
+})
+
+app.put('/produtos/:id', (req, res) => {
+    const produto = bancoDeDados.salvarProduto({
+        id: req.params.id,
+        nome: req.body.name,
+        preco: req.body.price
+    })
+    res.send(produto)
+})
+
+app.delete('/produtos/:id', (req, res) => {
+    res.status(204).send(bancoDeDados.excluirProduto(req.params.id))
 })
 
 app.listen(porta, () => {
