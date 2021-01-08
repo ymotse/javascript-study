9115f4f50084edd032fa631444baa1a29c1e58c9
Yitshhaq Fukushima <ymotse@gmail.com>
2021-01-08 17:06:39 -0300

    555. Soft Delete de Usuário

---
+++ knowledge/versao-inicial/backend/migrations/20210108165637_add_deleted_at_table_users.js
@@ -0,0 +1,13 @@
+const { KnexTimeoutError } = require("knex");
+
+exports.up = function(knex) {
+    return knex.schema.alterTable('users', table => {
+        table.timestamp('deletedAt')
+    })
+};
+
+exports.down = function(knex) {
+    return knex.schema.alterTable('users', table => {
+        table.dropColumn('deletedAt')
+    })
+};
